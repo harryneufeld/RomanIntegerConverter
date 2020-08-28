9ad4bd390ab801639b7cd67c1e05dd41b23ce2c5
Harry Neufeld <mail@harry-neufeld.de>
2020-08-28 13:46:27 +0200

    Added ConsoleApp;

---
+++ RomanIntegerConverter/RomanNumberConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using RomanIntegerConverter.Model;
+using System;
+
+namespace RomanNumberConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                RomanInteger x;
+                Console.WriteLine("Gib eine Ganzzahl ein:");
+                x = Int32.Parse(Console.ReadLine());
+                if (x >= 5000)
+                    Console.WriteLine("Achtung: Römische zahlen über 5000 können nicht in UTF8 dargestellt werden");
+                Console.WriteLine("Die römische Zahl ist: {0}", x.GetRomanNumber());
+                Console.WriteLine("----------------------------------------------");
+            }
+        }
+    }
+}
